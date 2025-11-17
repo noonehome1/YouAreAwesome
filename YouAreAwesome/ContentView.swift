2d2711c2accbeabbefca10237ccd611274993ad8
Paul Wagstaff <paul@pwfotos.ca>
2025-11-17 16:47:10 -0700

    Toggle Button Challenge

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -9,33 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var message = "I am a app developer!"
+    @State private var imageName = "sun.max.fill"
+    @State private var count = 1
+    
     var body: some View {
         VStack {
             Spacer()
-            Image(systemName: "swift")
+            Image(systemName: imageName)
                 .resizable()
                 .scaledToFit()
-                .foregroundStyle(.orange)
-                .frame(width: 200, height: 200 )
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
+//                .foregroundStyle(.orange)
+//                .frame(width: 200, height: 200 )
+//            Text(message)
+//                .font(.largeTitle)
+//                .fontWeight(.heavy)
+//                .foregroundStyle(.red)
             Spacer()
             HStack {
-                Button("Awesome!") {
-                    message = "Awesome!"
+                Button("Press Me!") {
+                    if (count % 2 == 0) {
+                        imageName = "moon.stars.fill"
+                        count += 1
+                    } else {
+                        imageName = "sun.max.fill"
+                        count += 1
+                    }
                 }
                 .buttonStyle(.borderedProminent)
                 .font(.title2)
                 .tint(.orange)
-                Button("Great!") {
-                    message = "Great!"
-                }
-                .buttonStyle(.borderedProminent)
-                .font(.title2)
-                .tint(.orange)
+//                Button("Great!") {
+//                    message = "Great!"
+//                }
+//                .buttonStyle(.borderedProminent)
+//                .font(.title2)
+//                .tint(.orange)
             }
+
         }
         .padding()
     }
