4812606063f4347bb2ec31939ff059aeb331281b
Paul Wagstaff <paul@pwfotos.ca>
2025-11-17 14:01:48 -0700

    Added our first button to the UI

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,16 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I am a app developer!"
     var body: some View {
         VStack {
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("You Are Awesome!")
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+            Button("Click Me!!") {
+                message = "Awesome!"
+            }
         }
         .padding()
     }
