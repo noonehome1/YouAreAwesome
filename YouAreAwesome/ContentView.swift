32e7140e3aa3057a0670db37a6e54a2ac848a2f2
Paul Wagstaff <paul@pwfotos.ca>
2025-11-17 14:24:47 -0700

    Added Button Styles and spacing

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -11,16 +11,30 @@ struct ContentView: View {
     @State private var message = "I am a app developer!"
     var body: some View {
         VStack {
+            Spacer()
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
+                .frame(width: 200, height: 200 )
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
-            Button("Click Me!!") {
-                message = "Awesome!"
+            Spacer()
+            HStack {
+                Button("Awesome!") {
+                    message = "Awesome!"
+                }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.orange)
+                Button("Great!") {
+                    message = "Great!"
+                }
+                .buttonStyle(.borderedProminent)
+                .font(.title2)
+                .tint(.orange)
             }
         }
         .padding()
